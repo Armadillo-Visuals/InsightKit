d6f3968ae93a44557f87cc421e93eef94db9e31d
Max Heubel <maxheubel@Maxs-MacBook-Pro.local>
2022-12-11 15:47:18 -0500

    max's changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,59 @@
-import React, { component } from 'react';
+import React, { Component, useState } from 'react';
 import './stylesheets/styles.css';
+import Graph from './components/Graph';
+import Main from './components/Main';
 
-const App = () => <h1>In the App component!</h1>;
-
-
+const App = () => {
+  const [state, setStateQuerey] = useState('');
+  const [disaster, setDisaster] = useState('');
+  const [graphData, setGraphData] = useState({
+    labels: ['Jun', 'Jul', 'Aug', 'Sept'],
+    datasets: [
+      {
+        id: 1,
+        label: 'pie',
+        data: [5, 6, 7, 8],
+      },
+      {
+        id: 2,
+        label: 'cupcake',
+        data: [3, 2, 1, 0],
+      },
+    ],
+  });
+  const handleClick = () => {
+    let state = document.getElementById('state').value;
+    let disaster = document.getElementById('disaster').value;
+    setStateQuerey(state);
+    setDisaster(disaster);
+    console.log(`/disaster/${state}/${disaster}`);
+    fetch(`/disaster/${state}/${disaster}`)
+      .then((response) => response.json())
+      .then((data) => setGraphData(data));
+    // let data = {
+    //     labels: ['Jun', 'Jul', 'Aug', 'Sept'],
+    //     datasets: [
+    //       {
+    //         id: 1,
+    //         label: 'icecream',
+    //         data: [5, 6, 7, 8],
+    //       },
+    //       {
+    //         id: 2,
+    //         label: 'pancakes',
+    //         data: [3, 2, 1, 0],
+    //       },
+    //     ],
+    //   }
+    //   setGraphData(data)
+  };
+  console.log('state', graphData);
+  return (
+    <div>
+      <h1>Environmental Tracker</h1>
+      <Main graphData={graphData} state={state} disaster={disaster} handleClick={handleClick} />
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+import React, { component } from 'react';
+import Graph from './Graph';
+import Search from './search';
+
+const Main = (props) => {
+  return (
+    <div>
+      <h2>In the Main component!</h2>
+      <Search handleClick={props.handleClick} />
+      <Graph graphData={props.graphData} state={props.state} disaster={props.disaster} />
+    </div>
+  );
+};
+
+export default Main;
