1dae7b36653150fd94f98fe4f23bfc77dd6a8baa
Josh <you@example.com>
2022-12-14 18:42:58 -0500

    more main container work

--- client/src/components/widgetGridContainer.jsx
+++ client/src/components/widgetGridContainer.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Widget from './widget';
+import '../stylesheets/widgetGridContainer.css';
 
 const WidgetGridContainer = () => {
   // axios request to get all the widgets assigned to the user
   // loop through the assigned widgets and populate widgets
   return (
-    <div className='widgetGridDiv'>
-      <Widget />
+    <div className='widgetGridWrapper'>
+      <div className='widgetGridContainer'>
+        <Widget />
+      </div>
     </div>
   );
 };
