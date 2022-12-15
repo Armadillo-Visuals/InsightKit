5187506587bdaffb5301a7afd867dd490e61c0eb
sakurakiyama <sakura.bowden@gmail.com>
2022-12-15 16:12:17 -0500

    adding tests for route integration

---
+++ __tests__/react.js
@@ -0,0 +1,3 @@
+import React from 'React';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
---
+++ __tests__/supertest.js
@@ -0,0 +1,80 @@
+const request = require('supertest');
+const server = 'http://localhost:3000';
+
+describe('Route integration', () => {
+
+  // test for undefined endpoint
+  describe('/error', () => {
+    describe('GET', () => {
+      it('responds with 404 status and json/application content type', () => {
+        return request(server)
+          .get('/error')
+          .expect('Content-type', /application\/json/)
+          .expect(404);
+      });
+    });
+  });
+
+  // test for get request to disasters over time
+  describe('/data', () => {
+    describe('GET', () => {
+      it('responds with a 200 status and a json/application content type', () => {
+        return request(server)
+          .get('/data/disasters-over-time/:state/:type')
+          .expect('Content-type', /application\/json/)
+          .expect(200);
+      });
+    });
+  });
+
+  // test for get request to carbon over time should go here 
+
+  // test for get request to populate a pie chart with percentage of each disaster type per state goes here
+  
+  // test for post request to /users/signup endpoint
+  describe('/users', () => {
+    describe('POST', () => {
+      it('responds with a 201 status and a json/application content type', () => {
+        return request(server)
+          .post('/users/signup')
+          .send({
+            firstName: 'TestFirstName5',
+            lastName: 'TestLastName5',
+            username: 'TestUsername5',
+            password: 'TestPassword5',
+          })
+          .expect('Content-type', /application\/json/)
+          .expect(201);
+      });
+      // test for post request to /users/login endpoint
+      it('responds with a 201 status and a json/application content type', () => {
+        return request(server)
+        .post('/users/login')
+        .send({
+          username: 'TestUsername',
+          password: 'TestPassword',
+        })
+        .expect('Content-type', /application\/json/)
+        .expect(201);
+      });
+    });
+    // test for patch request to /users/widget endpoint
+    describe('PATCH', () => {
+      it('responds with a 200 status and a json/application content type', () => {
+        return request(server)
+        .patch('/users/widget')
+        .send({ userID: 23,
+          graphType: 'line',
+          dataType: 'carbon',
+          parameter1: 'CA'
+        })
+        .expect('Content-type', /application\/json/)
+        .expect(200);
+      })
+    })
+
+  });
+
+      
+  
+});
